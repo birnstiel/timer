2b70d8a6395c08e3105364483d0bb6d0f17eae2e
Til Birnstiel <birnstiel@me.com>
2022-07-28 09:37:11 +0200

    added option to set end time

--- timer.py
+++ timer.py
@@ -6,7 +6,7 @@ import datetime
 import time
 import argparse
 
-RTHF   = argparse.RawTextHelpFormatter
+RTHF = argparse.RawTextHelpFormatter
 PARSER = argparse.ArgumentParser(description=__doc__, formatter_class=RTHF)
 PARSER.add_argument('hour', help='hour goal', nargs='?', type=int, default=0)
 PARSER.add_argument('minute', help='minute goal', nargs='?', type=int, default=0)
@@ -14,10 +14,15 @@ PARSER.add_argument('second', help='second goal', nargs='?', type=int, default=0
 PARSER.add_argument('-d', '--duration', help='duration in minutes', type=int, default=0)
 PARSER.add_argument('-f', '--font', help='art package font', type=str, default='big')
 PARSER.add_argument('-r', '--rate', help='refresh every RATE seconds', type=float, default=1)
-ARGS  = PARSER.parse_args()
+PARSER.add_argument('-e', '--end', help='show time remaining untils at this time (e.g. \'16:00\')', type=str, default=None)
+ARGS = PARSER.parse_args()
 
 if ARGS.duration > 0:
     goal = datetime.datetime.now() + datetime.timedelta(minutes=ARGS.duration)
+elif ARGS.end is not None:
+    goal = datetime.datetime.strptime(ARGS.end, "%H:%M")
+    goal = datetime.datetime.combine(datetime.datetime.today(), goal.time())
+
 else:
     goal = datetime.datetime.now().replace(hour=ARGS.hour, minute=ARGS.minute, second=ARGS.second)
 
